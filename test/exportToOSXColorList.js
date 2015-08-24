f1c245d7816b6bef3029f21074de2be1b8e43c5f
Diego Fernandez <diegof79@gmail.com>
2015-08-24 00:36:54 -0300

    Initial support to export CLR files

---
+++ test/exportToOSXColorList.js
@@ -0,0 +1,61 @@
+var exportToOSXColorList = require('../lib').exportToOSXColorList;
+var read = require('../lib').read;
+var assert = require('assert');
+var fs = require('fs');
+var path = require('path');
+var spawn = require('child_process').spawn;
+var onOSXIt = process.platform === 'darwin' ? it : it.skip;
+
+describe('exportToOSXColorList', function () {
+  var testCLR = path.resolve(__dirname, 'test-out.clr');
+
+  function file(fileName) {
+    return fs.readFileSync(path.resolve(__dirname, 'files/', fileName));
+  }
+
+  function clrDump(fileName, cb) {
+    var child = spawn('osascript', ['-l', 'JavaScript', '-']);
+    var result = new Buffer([]);
+    child.stdin.write("ObjC.import('Cocoa');\n");
+    child.stdin.write("var list = $.NSColorList.alloc.initWithNameFromFile('', './test/test-out.clr');\n");
+    child.stdin.write("JSON.stringify(ObjC.deepUnwrap(list.allKeys).map(function (e) {\n");
+    child.stdin.write("  var color = list.colorWithKey(e);\n");
+    child.stdin.write("  return {name: e, r: color.redComponent, g: color.greenComponent, b: color.blueComponent};\n");
+    child.stdin.write("}))\n");
+    child.stdin.end();
+    child.stdout.on('data', function (buff) {
+      result = Buffer.concat([result, buff]);
+    });
+    child.on('exit', function () {
+      cb(JSON.parse(result.toString()));
+    });
+  }
+
+  afterEach(function () {
+    fs.unlinkSync(testCLR);
+  });
+
+  onOSXIt('saves a CLR file from ASE file', function (done) {
+    exportToOSXColorList(
+        read(file('one-color.ase')),
+        testCLR,
+        'Test',
+        function (err) {
+          assert(!err);
+          clrDump(testCLR, function (out) {
+            assert.equal(out.length, 1);
+            assert.deepEqual(out[0], {name:'Red', r:1, g:0, b:0});
+            done();
+          });
+        });
+  });
+
+  onOSXIt('ignores group names');
+
+  onOSXIt('ignores unknown entries');
+
+  onOSXIt('supports CMYK colors');
+
+  onOSXIt('ignores LAB colors');
+
+});
